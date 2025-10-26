d1b5a8e04cd7f8caa44e8eae71ae46a186b2990b
Daniel <danielmateos684@gmail.com>
2025-10-26 14:59:23 +0100

    entregas

--- ut04/pr0402/archivos_python/pr402_6.py
+++ ut04/pr0402/archivos_python/pr402_6.py
@@ -1,2 +1,5 @@
 # 6. Mayúsculas y minúsculas
 # Escribe un programa que convierta las letras minúsculas de una cadena en mayúsculas y viceversa.
+text = input("Escribe una cadena a continuación: ")
+cadena_convertida = text.swapcase()
+print("Cadena convertida:", cadena_convertida)
--- ut04/pr0402/archivos_python/pr402_7.py
+++ ut04/pr0402/archivos_python/pr402_7.py
@@ -1,2 +1,6 @@
 # 7. Rotar caracteres de una cadena
 # Escribe una función que rote una cadena hacia la izquierda un número dado de veces.
+text = input("Escribe una cadena a continuación: ")
+n = 2
+rotada = text[n:] + text[:n]
+print(rotada)
