030f96973f8869f8ceadf61b99bfadc1a856a47b
Daniel <danielmateos684@gmail.com>
2025-10-31 16:38:37 +0100

    Listas

--- ut04/pr0403/archivos_python/pr0403_02_2.py
+++ ut04/pr0403/archivos_python/pr0403_02_2.py
@@ -1,2 +1,8 @@
 # Contar elementos específicos: 
 # Dada una lista de palabras, permite al usuario ingresar una palabra y cuenta cuántas veces aparece en la lista.
+
+palabra = input("Escribe una palabra: ")
+palabras = ["palabra1", "palabra1", "palabra1", "palabra1", "palabra1", "palabra1", "palabra2", "palabra2" ]
+cantidad = palabras.count(palabra)
+
+print(f"La palabra '{palabra}' aparece {cantidad} veces.")
---
+++ ut04/pr0403/archivos_python/pr0403_02_3.py
@@ -0,0 +1,7 @@
+# 11. Eliminar duplicados.
+# Dada una lista de números, elimina todos los elementos duplicados y muestra la lista con solo valores únicos.
+
+palabras = ["palabra1", "palabra1", "palabra1", "palabra1", "palabra1", "palabra1", "palabra2", "palabra2" ]
+sin_duplicados = set(palabras)
+
+print(sin_duplicados)
